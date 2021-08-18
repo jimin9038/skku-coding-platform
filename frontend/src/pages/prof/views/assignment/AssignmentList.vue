e0461bb655bfe8deab0fba4566a7b262eec8430b
DailyLinux <baksa1849@gmail.com>
2021-08-18 21:24:27 +0900

    Initialize AssignmentList.vue.

--- frontend/src/pages/prof/views/assignment/AssignmentList.vue
+++ frontend/src/pages/prof/views/assignment/AssignmentList.vue
@@ -1,6 +1,23 @@
 <template>
-  <!-- Please split into many componenets !-->
   <div>
+    <b-card title="Assignments">
+      <div class="table">
+        <b-table
+          hover
+          :items="assignmentlist"
+          :fields="assignmentfield"
+        >
+          <b-icon icon="caret-right-fill" aria-hidden="true"></b-icon>
+          <b-form-checkbox switch size="sm"></b-form-checkbox>
+          <b-icon icon="pencil" aria-hidden="true"></b-icon>
+          <b-icon icon="box-arrow-in-down" aria-hidden="true"></b-icon>
+          <b-icon icon="trash" aria-hidden="true"></b-icon>
+        </b-table>
+      </div>
+      <div v-if="!assignmentlist.length">
+        No Assignment
+      </div>
+    </b-card>
   </div>
 </template>
 
@@ -13,6 +30,9 @@ export default {
   },
   data () {
     return {
+      assignmentfield: ['Title', 'Visible', 'Status', 'Operation'],
+      assignmentlist: [
+      ]
     }
   },
   async mounted () {
@@ -25,5 +45,5 @@ export default {
 </script>
 
 <style lang="scss">
-  // Be careful of common css selector in admin/oj
+
 </style>
